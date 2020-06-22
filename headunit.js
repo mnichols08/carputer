2f485694f05f8c4a5cd72bac2698047515f8d5a5
Mike Nichols <mikenichols410@gmail.com>
2020-06-22 12:24:50 -0400

    inti

---
+++ headunit.js
@@ -0,0 +1,6 @@
+/*
+    7" 1024px x 600px Touchscreen Display
+    User Interface for controlling the three settings that exist already.
+    Displays information coming from outside sources such as the temperature and humidity  
+*/
+// Pseudo Code
