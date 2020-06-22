5e80f0462dbc547d6c8b56d3118b206c7856cf42
Mike Nichols <mikenichols410@gmail.com>
2020-06-22 12:33:56 -0400

    setup readme

--- headunit.js
+++
@@ -1,6 +0,0 @@
-/*
-    7" 1024px x 600px Touchscreen Display
-    User Interface for controlling the three settings that exist already.
-    Displays information coming from outside sources such as the temperature and humidity  
-*/
-// Pseudo Code
